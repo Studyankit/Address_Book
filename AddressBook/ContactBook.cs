dd1159b08d2d2cf9c406e6a378e5ae9179372f9e
StudyAnkit <ankitghosh2018@gmail.com>
2022-02-16 06:50:42 +0530

    Create a contact usng oops

--- AddressBook/ContactBook.cs
+++ AddressBook/ContactBook.cs
@@ -6,9 +6,8 @@ using System.Threading.Tasks;
 
 namespace AddressBook
 {
-    public class ContactBook
+    class ContactBook
     {
-        //properties
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
@@ -18,13 +17,37 @@ namespace AddressBook
         public double PhoneNumber { get; set; }
         public string Email { get; set; }
 
-        //Method to display Contact.
-        public void displayContact()
+        /// <summary>
+        /// display contact information.
+        /// </summary>
+        public void DisplayContact()
         {
-            Console.WriteLine("FirstName: " + this.FirstName + " LastName: " + this.LastName
-                            + " Address: " + this.Address + " City: " + this.City + " State: "
-                            + this.State + " Zip: " + this.Zip + " PhoneNumber: "
-                            + this.PhoneNumber + " Email: " + this.Email);
+            Console.WriteLine("FirstName: " + this.FirstName + " LastName: " + this.LastName + " Address: " + this.Address +
+                                " City: " + this.City + " State: " + this.State + " Zip: " + this.Zip +
+                                  " PhoneNumber: " + this.PhoneNumber + " Email: " + this.Email);
+
+        }
+
+        /// <summary>
+        /// This method is used to add new contacts.
+        /// </summary>
+        public void AddContact()
+        {
+            Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
+            ContactBook contactPerson = new ContactBook()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToDouble(Console.ReadLine()),
+                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
+                Email = Console.ReadLine(),
+            };
+            List<ContactBook> list = new List<ContactBook>();
+            list.Add(contactPerson);
+            contactPerson.DisplayContact();
         }
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -2,30 +2,17 @@
 using AddressBook;
 
 
-namespace AddressBookSystem
+using System;
+using AddressBook;
+
+namespace AddressBook
 {
-    public class Program
+    class Program
     {
-        //Main  Method / program Entry Point
         static void Main(string[] args)
         {
-            //Showing Welcome Message On console.
-            Console.WriteLine("Welcome to Address Book Progam \n");
-
-            Console.Write("Enter First Name, Last Name, Address, City, State, Zip, Phone Number, Email \n");
-            ContactBook addressBook = new ContactBook()  //Initializing elements using collection-initializer syntax
-            {
-                FirstName = Console.ReadLine(),
-                LastName = Console.ReadLine(),
-                Address = Console.ReadLine(),
-                City = Console.ReadLine(),
-                State = Console.ReadLine(),
-                Zip = Convert.ToDouble(Console.ReadLine()),
-                PhoneNumber = Convert.ToDouble(Console.ReadLine()),
-                Email = Console.ReadLine(),
-            };
-            addressBook.displayContact();
-            Console.ReadLine();
+            ContactBook contactPerson = new ContactBook();
+            contactPerson.AddContact();
         }
     }
 }
